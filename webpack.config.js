7b75335e207ff7cdbe58f098866514f9f9a71402
Romulo Messias <romulomessias@me.com>
2019-06-29 20:28:54 -0300

    added support to pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 const sourcePath = path.join(__dirname, './src');
 const outputhPath = path.resolve(__dirname, './dist');
@@ -35,6 +35,30 @@ const webpackConfig = {
         new WorkboxWebpackPlugin.GenerateSW({
             clientsClaim: true,
             importWorkboxFrom: 'cdn',
+        }),
+        new WebpackPwaManifest({
+            name: 'My Progressive Web App',
+            short_name: 'MyPWA',
+            description: 'My awesome Progressive Web App!',
+            background_color: '#fff',
+            theme_color: '#fff',
+            display: 'standalone',
+            start_url: ".",
+            orientation: "portrait",
+            icons: [
+                {
+                    src: path.resolve('src/assets/icon.png'),
+                    sizes: [192, 256, 512],
+                    ios: true
+                }
+            ],
+            ios: {
+                // 'apple-touch-icon': string | IosAppleTouchIcon,
+                // 'apple-touch-startup-image': string,
+                'apple-mobile-web-app-title': 'MyPWA',
+                'apple-mobile-web-app-capable': true,
+                'apple-mobile-web-app-status-bar-style':  'black'
+            },
         }),
     ],
     node: {
