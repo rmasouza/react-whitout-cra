8492e67f0ae664f4bcf8128c9890a3eb29f2dffc
Romulo Messias <romulomessias@me.com>
2019-08-10 17:04:16 -0300

    added support for dotenv, simplified service worker resgistrations, added sizes compression and other changes in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 'use strict';
 const path = require('path');
 const webpack = require('webpack');
+const compression = require('compression');
+const dotenv = require('dotenv');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest')
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const Dotenv = require('dotenv-webpack');
 
 const sourcePath = path.join(__dirname, './src');
 const outputhPath = path.resolve(__dirname, './dist');
 
+dotenv.config();
+console.log(process.env.NODE_ENV);
+
 const webpackConfig = {
     context: sourcePath,
     entry: {
-        app:'./index.tsx'
+        main:'./index.tsx',
     },
     output: {
         path: outputhPath,
-        filename: "bundle.js"
+        filename: "[name].[chunkhash].js",
+        chunkFilename: '[name]-[chunkhash].js'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']
@@ -27,20 +38,68 @@ const webpackConfig = {
             { test: /\.css$/, use: [ 'bstyle-loader', 'css-loader' ] },
         ]
     },
-    mode: 'development',
+    mode: process.env.NODE_ENV,
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                test: /\.js(\?.*)?$/i,
+            }),
+        ],
+        runtimeChunk: true,
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                reactVendors: {
+                    test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+                    name: "react~vendors"
+                },
+                //     vendor: {
+                //         test: /[\\/]node_modules[\\/]/,
+                //         name(module) {
+                //             // get the name. E.g. node_modules/packageName/not/this/part.js
+                //             // or node_modules/packageName
+                //             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                //             // npm package names are URL-safe, but some servers don't like @ symbols
+                //             return `npm.${packageName.replace('@', '')}`;
+                //         },
+                //     },
+            }
+        },
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "assets/index.html",
-            favicon: 'assets/icon.ico'
+            favicon: 'assets/icon.ico',
+            inlineSource: 'runtime~.+\\.js',
         }),
+        new InlineSourcePlugin(),
         new WorkboxWebpackPlugin.GenerateSW({
             clientsClaim: true,
             importWorkboxFrom: 'cdn',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https?.*/,
+                    handler: 'NetworkFirst',
+                    options: {
+                        cacheName: 'MyPwaCache',
+                        expiration: {
+                            maxEntries: 200
+                        }
+                    }
+                }
+            ],
+        }),
+        new CompressionPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
         }),
         new WebpackPwaManifest({
             name: 'My Progressive Web App',
             short_name: 'MyPWA',
-            description: 'My awesome Progressive Web App!',
+            description: 'My Awesome Progressive Web App!',
             background_color: '#fff',
             theme_color: '#fff',
             display: 'standalone',
@@ -61,6 +120,10 @@ const webpackConfig = {
                 'apple-mobile-web-app-status-bar-style':  'black'
             },
         }),
+        new Dotenv({
+            path: path.join(__dirname, './.env'),
+            systemvars: true
+        })
     ],
     node: {
         module: 'empty',
@@ -72,6 +135,12 @@ const webpackConfig = {
         tls: 'empty',
         child_process: 'empty',
     },
+    devServer: {
+        compress: true,
+        before(app) {
+            app.use(compression({}));
+        },
+    }
 };
 
 module.exports=webpackConfig;
