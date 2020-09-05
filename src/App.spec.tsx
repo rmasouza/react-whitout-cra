7dfcaea968568e521972d09ef43136881dd43f33
Romulo Messias <romulomessias@me.com>
2020-09-05 20:15:47 -0300

    refactor: changed test config file

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
-
     test('renders App component', () => {
         render(<App />);
     });
--- test/jest.config.js
+++ test/jest.config.js
@@ -1,21 +1,13 @@
 module.exports = {
     rootDir: '../',
     moduleFileExtensions: ['json', 'ts', 'tsx', 'js', 'jsx'],
-    setupFiles: ['<rootDir>/test/jest.setup.js'],
     moduleNameMapper: {
         '^src(.*)$': '<rootDir>/src$1',
         '^test(.*)$': '<rootDir>/test$1',
     },
     transform: {
         '\\.tsx?$': 'ts-jest',
-        '\\.jsx?$': 'babel-jest',
     },
-    globals: {
-        'ts-jest': {
-            tsConfig: '<rootDir>/tsconfig.json',
-        },
-    },
-    // preset: 'ts-jest',
     testRegex: '.spec.(tsx?|ts?)$',
     testPathIgnorePatterns: ['../node_modules/'],
 };
