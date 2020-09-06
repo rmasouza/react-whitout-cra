0f163e8a739391948cb1ed686b1781183ee856a9
Romulo Messias <romulomessias@me.com>
2020-09-05 22:43:46 -0300

    chore: renamed test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
-    test('renders App component', () => {
+    test('renders App component successfully', () => {
         render(<App />);
     });
 });
