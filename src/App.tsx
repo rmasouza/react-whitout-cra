02eb6970aad4915d2b9de80869368d63a45ad669
Romulo Messias <romulomessias@me.com>
2020-09-06 11:31:02 -0300

    feat: added a hook to add some border effect to a html element, also updated tsconfig and component test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import useIsOnline from './hooks/network/useIsOnline';
 
+import './App.scss';
+import useGradientBorderEffect from './hooks/stylesEffect/useGradientBorderEffect';
 
 const App: React.FC = () => {
     const isOnline = useIsOnline();
+    const { onMouseMove, elementRef, background } = useGradientBorderEffect('#3acfd5', '#3a4ed5');
 
     return (
-        <article>
-            <header>Hello World!</header>
-            <section>
-                {
-                    isOnline ? 'we are online!' : 'we are not online :('
-                }
+        <article
+          className="app__container"
+          ref={elementRef}
+          onMouseMove={onMouseMove}
+          style={{ background }}
+        >
+            <section className="app">
+                <header>Hello World!</header>
+                <p>
+                    {
+                        isOnline ? 'we are online!' : 'we are not online :('
+                    }
+                </p>
             </section>
         </article>
     );
--- test/jest.config.js
+++ test/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     moduleNameMapper: {
         '^src(.*)$': '<rootDir>/src$1',
         '^test(.*)$': '<rootDir>/test$1',
+        '\\.(css|sass|scss)$': '<rootDir>/test/__mocks__/styles.js',
     },
     transform: {
         '\\.tsx?$': 'ts-jest',
