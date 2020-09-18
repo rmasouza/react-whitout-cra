e8bba09c79aa9656511a04ffc829fc2d200eebdb
Romulo Messias <romulomessias@me.com>
2020-09-18 19:38:15 -0300

    feat: changed style load for mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const WorkerPlugin = require('worker-plugin');
 const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const sourcePath = path.join(__dirname, './src');
 const outputhPath = path.resolve(__dirname, './dist');
@@ -19,31 +20,39 @@ dotenv.config();
 const webpackConfig = {
     context: sourcePath,
     mode: process.env.NODE_ENV,
-    devtool: 'source-map',
+    devtool: "source-map",
     entry: {
-        main: './index.tsx',
+        main: "./index.tsx",
     },
     output: {
         path: outputhPath,
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name]-[chunkhash].js',
+        filename: "[name].[chunkhash].js",
+        chunkFilename: "[name]-[chunkhash].js",
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
+        extensions: [".ts", ".tsx", ".js", ".json"],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/,
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                includePaths: [`${sourcePath}/styles`],
+                            },
+                            sourceMap: true,
+                        },
+                    },
                 ],
             },
         ],
@@ -56,32 +65,35 @@ const webpackConfig = {
         ],
         runtimeChunk: true,
         splitChunks: {
-            chunks: 'all',
+            chunks: "all",
             maxInitialRequests: Infinity,
             minSize: 0,
             cacheGroups: {
                 reactVendors: {
                     test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-                    name: 'react~vendors',
+                    name: "react~vendors",
                 },
             },
         },
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'assets/index.html',
-            favicon: 'assets/icon.ico',
-            inlineSource: 'runtime~.+\\.js',
+            template: "assets/index.html",
+            favicon: "assets/icon.ico",
+            inlineSource: "runtime~.+\\.js",
         }),
-        // new InlineSourcePlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].[chunkhash].css",
+            chunkFilename: "[id].css",
+        }),
         new WorkboxWebpackPlugin.GenerateSW({
             clientsClaim: true,
             runtimeCaching: [
                 {
                     urlPattern: /^https?.*/,
-                    handler: 'NetworkFirst',
+                    handler: "NetworkFirst",
                     options: {
-                        cacheName: 'MyPwaCache',
+                        cacheName: "MyPwaCache",
                         expiration: {
                             maxEntries: 200,
                         },
@@ -91,46 +103,46 @@ const webpackConfig = {
         }),
         new CompressionPlugin(),
         new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
+            analyzerMode: "static",
             openAnalyzer: false,
         }),
         new WebpackPwaManifest({
-            name: 'My Progressive Web App',
-            short_name: 'MyPWA',
-            description: 'My Awesome Progressive Web App!',
-            background_color: '#fff',
-            theme_color: '#fff',
-            display: 'standalone',
-            start_url: '.',
-            orientation: 'portrait',
+            name: "My Progressive Web App",
+            short_name: "MyPWA",
+            description: "My Awesome Progressive Web App!",
+            background_color: "#fff",
+            theme_color: "#fff",
+            display: "standalone",
+            start_url: ".",
+            orientation: "portrait",
             icons: [
                 {
-                    src: path.resolve('src/assets/icon.png'),
+                    src: path.resolve("src/assets/icon.png"),
                     sizes: [192, 256, 512],
                     ios: true,
                 },
             ],
             ios: {
-                'apple-mobile-web-app-title': 'MyPWA',
-                'apple-mobile-web-app-capable': true,
-                'apple-mobile-web-app-status-bar-style': 'black',
+                "apple-mobile-web-app-title": "MyPWA",
+                "apple-mobile-web-app-capable": true,
+                "apple-mobile-web-app-status-bar-style": "black",
             },
         }),
         new Dotenv({
-            path: path.join(__dirname, './.env'),
+            path: path.join(__dirname, "./.env"),
             systemvars: true,
         }),
         new WorkerPlugin(),
     ],
     node: {
-        module: 'empty',
-        dgram: 'empty',
-        dns: 'mock',
-        fs: 'empty',
-        http2: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty',
+        module: "empty",
+        dgram: "empty",
+        dns: "mock",
+        fs: "empty",
+        http2: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty",
     },
     devServer: {
         compress: true,
