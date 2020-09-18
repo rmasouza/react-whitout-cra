bd27cff06b7181b144304dfd38cc4cb50da07da3
Romulo Messias <romulomessias@me.com>
2020-09-18 19:31:16 -0300

    feat: added React Strict

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 // import { register } from './utils/ServiceWorker';
 
-import './styles/index.scss';
+import "./styles/index.scss";
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root'),
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // register();
